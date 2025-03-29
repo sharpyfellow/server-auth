34812ba02278c7f11e01bb5c5768cc06e14959db
Trond Lind Hansen <trondlindhansen@Tronds-MacBook-Pro.local>
2025-03-29 21:04:09 +0100

    La til isAdmin i brukermodell

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
   profileImageUrl: { type: String },
+  isAdmin: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model("User", userSchema);
