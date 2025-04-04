2fba2fe7df2bc40965a04bb24ba1904b3c7aaab0
Trond Lind Hansen <trondlindhansen@Tronds-MacBook-Pro.local>
2025-04-04 23:23:49 +0200

    La til likes-felt i Post-modellen

--- models/Post.js
+++ models/Post.js
@@ -1,27 +1,21 @@
 const mongoose = require("mongoose");
 
+const commentSchema = new mongoose.Schema(
+  {
+    text: String,
+    commentedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
 const postSchema = new mongoose.Schema(
   {
     title: String,
     description: String,
     imageUrl: String,
-    postedBy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-    comments: [
-      {
-        text: String,
-        commentedBy: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "User",
-        },
-        createdAt: {
-          type: Date,
-          default: Date.now,
-        },
-      },
-    ],
+    postedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    comments: [commentSchema],
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   },
   { timestamps: true }
 );
